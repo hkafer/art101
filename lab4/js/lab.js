cf212e69376a423d8c0cf5167d7ab7643fedd638
hkafer <hkafer@ucsc.edu>
2024-10-17 17:50:46 -0700

    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,17 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - How to make avocado toast.
+// Author: Hailey Kafer <hkafer@ucsc.edu>
+// Date: October 2024
 
-// Constants
+// Take bread out of the refrigerator.
+// Toast bread in a toaster for the desired time. 
+// Cut avocado into slices. 
+// Place toast on a plate. 
+// Spread avocado slices onto toast.
+// Sprinkle on everything bagel seasoning on toast.
 
-// Functions
+// How to play Snake.
+// Guide the snake to the apple in a left, right, down, or up direction.
+// After the snake eats the apple it grows by one.
+// Avoid walls and the snake’s own body while collecting apples.
+// The game continues until the snake runs into itself or a wall, or if there is no space left.
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
